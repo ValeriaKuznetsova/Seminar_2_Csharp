68b02cec3a127a39b3649e044a19535188866988
Valeria <valeriakuz92@mail.ru>
2022-11-20 14:36:42 +0500

    Сохранение задачи

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Console.WriteLine ("Enter the number from 100 to 999: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// if (number < 100 || number > 999)
+// {
+// do
+// {
+// Console.WriteLine ("Wrong number!Enter the number from 100 to 999: ");
+// Int32.TryParse(Console.ReadLine(), out number);
+// } while (number < 100 || number > 999);
+// }
+// number = number/10;
+// int SecondNumber = number % 10;
+// Console.WriteLine("The second number is " + SecondNumber);
